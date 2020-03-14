76d7002fdda450f8f88319f692f32595f9168147
Debora Alves <foonatic@gmail.com>
2020-03-14 16:31:57 -0300

    pagina finalizada

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72")
   .then(response => response.json())
   .then((data) => {
-	const string = ""
+	let string = ""
     data.forEach((acomodacao) => {
 	string += `
-		<img src="${acomodocao.photo}"/>
-		<p class="property_type">${acomodocao.property_type}</p>
-		<p class="name">${acomodocao.name}</p>
-		<p class="price">${acomodocao.price}</p>
+		<div class="card">
+			<img src="${acomodacao.photo}"/>
+			<p class="property_type">${acomodacao.property_type}</p>
+			<p class="name">${acomodacao.name}</p>
+			<p class="price"> R$ ${acomodacao.price} / noite</p>
+		</div>
 	`
 });
 	document.querySelector(".cards-div").innerHTML = string;
 });
-
-export { string };
 
 
 
